59293c1d664c4a6e98e4a7ce1de48770bf54fc27
Jesse Lisser <jesselisser@gmail.com>
2019-12-14 13:48:21 +0100

    initial commit

---
+++ 1-fuel.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+
+const buffer = fs.readFileSync('./1_input.txt');
+const moduleWeights = buffer.toString().trim().split('\r\n').map(Number);
+
+const massToFuel = (mass) => Math.floor(mass / 3) - 2;
+const calculateExtraFuel = (remainingFuel) => {
+  const fuelRequired = massToFuel(remainingFuel);
+  return fuelRequired <= 0 ? 0 : fuelRequired + calculateExtraFuel(fuelRequired);
+}
+const totalMassToFuel = (mass) => {
+  const initialFuel = massToFuel(mass);
+  const extraFuel = calculateExtraFuel(initialFuel);
+  return initialFuel + extraFuel;
+}
+
+const test4 = totalMassToFuel(1969)
+test4
+
+/* Fuel required for modules */
+const fuelRequiredModules = moduleWeights
+  .map(totalMassToFuel)
+  .reduce((a, b) => a + b, 0);
+fuelRequiredModules
